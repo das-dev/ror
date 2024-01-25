a0e77fa05f84e6266546f2b840d1c49f605b22ad
das <das.dev@pm.me>
2024-01-26 02:30:46 +0600

    Lesson 4 | refactor tests

--- tests/test_railway_station_manager.rb
+++ tests/test_railway_station_manager.rb
@@ -26,11 +26,11 @@ class TestCase
 end
 
 module TestStationHelper
-  def make_station
+  def station
     Station.new('Moscow')
   end
 
-  def make_train
+  def train
     Train.new('123', :passenger, 5)
   end
 end
@@ -47,7 +47,7 @@ module TestRouteHelper
     @destination_station = Station.new('Sankt-Peterburg-Glavn')
   end
 
-  def make_route
+  def route
     Route.new(origin_station, destination_station)
   end
 
@@ -80,11 +80,11 @@ module TestTrainHelper
     Route.new(destination_station, origin_station)
   end
 
-  def make_train
+  def train
     Train.new('123', :passenger, 5)
   end
 
-  def make_route
+  def route
     Route.new(origin_station, destination_station)
   end
 end
@@ -101,20 +101,20 @@ class TestStationCase < TestCase
   end
 
   def test_name_station
-    station = make_station
+    station = self.station
 
     assert_equal 'Moscow', station.name
   end
 
   def test_empty_station
-    station = make_station
+    station = self.station
 
     assert_equal [], station.trains
   end
 
   def test_add_train
-    station = make_station
-    train = make_train
+    station = self.station
+    train = self.train
 
     station.add_train(train)
     station.add_train(train)
@@ -123,8 +123,8 @@ class TestStationCase < TestCase
   end
 
   def test_send_train
-    station = make_station
-    train = make_train
+    station = self.station
+    train = self.train
     station.add_train(train)
 
     station.send_train(train)
@@ -134,8 +134,8 @@ class TestStationCase < TestCase
   end
 
   def test_train_types_stat
-    station = make_station
-    train = make_train
+    station = self.station
+    train = self.train
     station.add_train(train)
 
     assert_equal({ passenger: 1 }, station.train_types_stat)
@@ -158,7 +158,7 @@ class TestRouteCase < TestCase
   end
 
   def test_initial_route_state
-    route = make_route
+    route = self.route
 
     assert_equal origin_station, route.origin_station
     assert_equal destination_station, route.destination_station
@@ -166,7 +166,7 @@ class TestRouteCase < TestCase
   end
 
   def test_intermediate_station_order
-    route = make_route
+    route = self.route
 
     append_stations(route)
 
@@ -174,7 +174,7 @@ class TestRouteCase < TestCase
   end
 
   def test_append_double_station
-    route = make_route
+    route = self.route
 
     route.append_intermediate_station(intermediate_station1)
     route.append_intermediate_station(intermediate_station1)
@@ -183,7 +183,7 @@ class TestRouteCase < TestCase
   end
 
   def test_remove_intermediate_station
-    route = make_route
+    route = self.route
     append_stations(route)
 
     route.remove_intermediate_station(intermediate_station1)
@@ -194,7 +194,7 @@ class TestRouteCase < TestCase
   end
 
   def test_remove_missing_intermediate_station
-    route = make_route
+    route = self.route
 
     route.remove_intermediate_station(origin_station)
     route.remove_intermediate_station(intermediate_station1)
@@ -221,7 +221,7 @@ class TestTrainCase < TestCase
   end
 
   def test_initial_train_state
-    train = make_train
+    train = self.train
 
     assert_equal '123', train.number
     assert_equal :passenger, train.type
@@ -231,7 +231,7 @@ class TestTrainCase < TestCase
   end
 
   def test_speed_up
-    train = make_train
+    train = self.train
 
     train.speed_up(10)
 
@@ -239,7 +239,7 @@ class TestTrainCase < TestCase
   end
 
   def test_speed_down
-    train = make_train
+    train = self.train
 
     train.speed_up(10)
     train.speed_down(5)
@@ -248,7 +248,7 @@ class TestTrainCase < TestCase
   end
 
   def test_full_stop
-    train = make_train
+    train = self.train
 
     train.speed_up(10)
     train.speed_down(15)
@@ -257,7 +257,7 @@ class TestTrainCase < TestCase
   end
 
   def test_attach_carriage
-    train = make_train
+    train = self.train
 
     train.attach_carriage
 
@@ -265,7 +265,7 @@ class TestTrainCase < TestCase
   end
 
   def test_detach_carriage
-    train = make_train
+    train = self.train
 
     train.detach_carriage
 
@@ -273,7 +273,7 @@ class TestTrainCase < TestCase
   end
 
   def test_carriage_change_while_moving
-    train = make_train
+    train = self.train
 
     train.speed_up(10)
 
@@ -310,8 +310,8 @@ class TestTrainOnRouteCase < TestCase
   end
 
   def test_assign_route
-    train = make_train
-    route = make_route
+    train = self.train
+    route = self.route
 
     train.assign_route(route)
 
@@ -320,8 +320,8 @@ class TestTrainOnRouteCase < TestCase
   end
 
   def test_move_forward
-    train = make_train
-    route = make_route
+    train = self.train
+    route = self.route
     train.assign_route(route)
 
     train.move_forward
@@ -330,8 +330,8 @@ class TestTrainOnRouteCase < TestCase
   end
 
   def test_move_backward
-    train = make_train
-    route = make_route
+    train = self.train
+    route = self.route
     train.assign_route(route)
     train.move_forward
 
@@ -341,8 +341,8 @@ class TestTrainOnRouteCase < TestCase
   end
 
   def test_move_forward_when_last
-    train = make_train
-    route = make_route
+    train = self.train
+    route = self.route
     train.assign_route(route)
     train.move_forward
 
@@ -352,33 +352,34 @@ class TestTrainOnRouteCase < TestCase
   end
 
   def test_reassign_route
-    train = make_train
-    route = make_route
+    train = self.train
+    route = self.route
     train.assign_route(route)
     train.move_forward
 
+    reverse_route_train = self.reverse_route_train
     train.assign_route(reverse_route_train)
 
     assert_equal reverse_route_train, train.route
   end
 
   def test_current_station
-    train = make_train
-    route = make_route
+    train = self.train
+    route = self.route
     train.assign_route(route)
 
     assert_equal origin_station, train.current_station
   end
 
   def test_current_station_without_route
-    train = make_train
+    train = self.train
 
     assert_equal nil, train.current_station
   end
 
   def test_previous_station
-    train = make_train
-    route = make_route
+    train = self.train
+    route = self.route
     train.assign_route(route)
     train.move_forward
 
@@ -386,16 +387,16 @@ class TestTrainOnRouteCase < TestCase
   end
 
   def test_next_station
-    train = make_train
-    route = make_route
+    train = self.train
+    route = self.route
     train.assign_route(route)
 
     assert_equal destination_station, train.next_station
   end
 
   def test_next_station_when_last
-    train = make_train
-    route = make_route
+    train = self.train
+    route = self.route
     train.assign_route(route)
     train.move_forward
 
@@ -403,8 +404,8 @@ class TestTrainOnRouteCase < TestCase
   end
 
   def test_previous_station_when_first
-    train = make_train
-    route = make_route
+    train = self.train
+    route = self.route
     train.assign_route(route)
 
     assert_equal nil, train.previous_station
