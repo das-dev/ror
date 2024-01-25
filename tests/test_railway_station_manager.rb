038e218054d6d7367749707cf377c626d770d709
das <das.dev@pm.me>
2024-01-26 01:09:06 +0600

    Lesson 4 | refactor tests

--- tests/test_railway_station_manager.rb
+++ tests/test_railway_station_manager.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative 'railway_station_manager'
+require_relative '../railway_station_manager/railway_station_manager'
 
 # rubocop:disable Style/Documentation
 class TestCase
@@ -36,34 +36,52 @@ module TestStationHelper
 end
 
 module TestRouteHelper
+  attr_reader :origin_station, :destination_station,
+              :intermediate_station1, :intermediate_station2, :intermediate_station3
+
+  def make_stations
+    @origin_station = Station.new('Moskva Oktyabrskaya')
+    @intermediate_station1 = Station.new('Tver')
+    @intermediate_station2 = Station.new('Vyshniy Volochek')
+    @intermediate_station3 = Station.new('Malaya Vishera')
+    @destination_station = Station.new('Sankt-Peterburg-Glavn')
+  end
+
   def make_route
-    Route.new(@origin_station, @destination_station)
+    Route.new(origin_station, destination_station)
   end
 
   def append_stations(route)
-    route.append_intermediate_station(@tver_station)
-    route.append_intermediate_station(@vyshniy_volochek_station)
-    route.append_intermediate_station(@malaya_vishera_station)
+    route.append_intermediate_station(intermediate_station1)
+    route.append_intermediate_station(intermediate_station2)
+    route.append_intermediate_station(intermediate_station3)
   end
 
   def all_stations
     [
-      @origin_station,
-      @tver_station,
-      @vyshniy_volochek_station,
-      @malaya_vishera_station,
-      @destination_station
+      origin_station,
+      intermediate_station1,
+      intermediate_station2,
+      intermediate_station3,
+      destination_station
     ]
   end
 end
 
 module TestTrainHelper
+  attr_reader :origin_station, :destination_station
+
+  def make_stations
+    @origin_station = Station.new('Moskva Oktyabrskaya')
+    @destination_station = Station.new('Sankt-Peterburg-Glavn')
+  end
+
   def make_train
     Train.new('123', :passenger, 5)
   end
 
   def make_route
-    Route.new(@origin_station, @destination_station)
+    Route.new(origin_station, destination_station)
   end
 end
 
@@ -72,6 +90,7 @@ class TestStationCase < TestCase
 
   def run
     test_name_station
+    test_empty_station
     test_add_train
     test_send_train
     test_types_trains
@@ -127,22 +146,19 @@ class TestRouteCase < TestCase
     test_intermediate_station_order
     test_append_double_station
     test_remove_intermediate_station
+    test_remove_missing_intermediate_station
   end
 
   def setup
-    @origin_station = Station.new('Moskva Oktyabrskaya')
-    @tver_station = Station.new('Tver')
-    @vyshniy_volochek_station = Station.new('Vyshniy Volochek')
-    @malaya_vishera_station = Station.new('Malaya Vishera')
-    @destination_station = Station.new('Sankt-Peterburg-Glavn')
+    make_stations
   end
 
   def test_initial_route_state
     route = make_route
 
-    assert_equal @origin_station, route.origin_station
-    assert_equal @destination_station, route.destination_station
-    assert_equal [@origin_station, @destination_station], route.stations
+    assert_equal origin_station, route.origin_station
+    assert_equal destination_station, route.destination_station
+    assert_equal [origin_station, destination_station], route.stations
   end
 
   def test_intermediate_station_order
@@ -156,30 +172,30 @@ class TestRouteCase < TestCase
   def test_append_double_station
     route = make_route
 
-    route.append_intermediate_station(@tver_station)
-    route.append_intermediate_station(@tver_station)
+    route.append_intermediate_station(intermediate_station1)
+    route.append_intermediate_station(intermediate_station1)
 
-    assert_equal [@origin_station, @tver_station, @destination_station], route.stations
+    assert_equal [origin_station, intermediate_station1, destination_station], route.stations
   end
 
   def test_remove_intermediate_station
     route = make_route
     append_stations(route)
 
-    route.remove_intermediate_station(@tver_station)
-    route.remove_intermediate_station(@malaya_vishera_station)
-    route.remove_intermediate_station(@vyshniy_volochek_station)
+    route.remove_intermediate_station(intermediate_station1)
+    route.remove_intermediate_station(intermediate_station3)
+    route.remove_intermediate_station(intermediate_station2)
 
-    assert_equal [@origin_station, @destination_station], route.stations
+    assert_equal [origin_station, destination_station], route.stations
   end
 
   def test_remove_missing_intermediate_station
     route = make_route
 
-    route.remove_intermediate_station(@origin_station)
-    route.remove_intermediate_station(@tver_station)
+    route.remove_intermediate_station(origin_station)
+    route.remove_intermediate_station(intermediate_station1)
 
-    assert_equal [@origin_station, @destination_station], route.stations
+    assert_equal [origin_station, destination_station], route.stations
   end
 end
 
@@ -197,8 +213,7 @@ class TestTrainCase < TestCase
   end
 
   def setup
-    @origin_station = Station.new('Moskva Oktyabrskaya')
-    @destination_station = Station.new('Sankt-Peterburg-Glavn')
+    make_stations
   end
 
   def test_initial_train_state
@@ -286,6 +301,10 @@ class TestTrainOnRouteCase < TestCase
   end
   # rubocop:enable Metrics/MethodLength
 
+  def setup
+    make_stations
+  end
+
   def test_assign_route
     train = make_train
     route = make_route
@@ -293,7 +312,7 @@ class TestTrainOnRouteCase < TestCase
     train.assign_route(route)
 
     assert_equal route, train.route
-    assert_equal @origin_station, train.current_station
+    assert_equal origin_station, train.current_station
   end
 
   def test_move_forward
@@ -303,7 +322,7 @@ class TestTrainOnRouteCase < TestCase
 
     train.move_forward
 
-    assert_equal @destination_station, train.current_station
+    assert_equal destination_station, train.current_station
   end
 
   def test_move_backward
@@ -314,7 +333,7 @@ class TestTrainOnRouteCase < TestCase
 
     train.move_backward
 
-    assert_equal @origin_station, train.current_station
+    assert_equal origin_station, train.current_station
   end
 
   def test_move_forward_when_last
@@ -334,7 +353,7 @@ class TestTrainOnRouteCase < TestCase
     train.assign_route(route)
     train.move_forward
 
-    new_route = Route.new(@destination_station, @origin_station)
+    new_route = Route.new(destination_station, origin_station)
     train.assign_route(new_route)
 
     assert_equal new_route, train.route
@@ -345,7 +364,7 @@ class TestTrainOnRouteCase < TestCase
     route = make_route
     train.assign_route(route)
 
-    assert_equal @origin_station, train.current_station
+    assert_equal origin_station, train.current_station
   end
 
   def test_current_station_without_route
@@ -360,7 +379,7 @@ class TestTrainOnRouteCase < TestCase
     train.assign_route(route)
     train.move_forward
 
-    assert_equal @origin_station, train.previous_station
+    assert_equal origin_station, train.previous_station
   end
 
   def test_next_station
@@ -368,7 +387,7 @@ class TestTrainOnRouteCase < TestCase
     route = make_route
     train.assign_route(route)
 
-    assert_equal @destination_station, train.next_station
+    assert_equal destination_station, train.next_station
   end
 
   def test_next_station_when_last
