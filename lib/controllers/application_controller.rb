4c60bc0f190e6a4ff47e98e34f6677c65afa24de
das <das.dev@pm.me>
2024-01-31 04:11:56 +0600

    Lesson 5 | compress "about app" screen

--- lib/controllers/application_controller.rb
+++ lib/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'base64'
+require 'zlib'
 
 require_relative '../model/station'
 
@@ -12,26 +13,14 @@ class ApplicationController
 
   def about
     content = <<~ABOUT
-      ICAgICAgICAgICAgICAgICAgXy09PT09LV9fLT09PT09PS1fXy09PT09PT09
-      PS1fX19fXy09PT09PT09PT09PT0tX18KICAgICAgICAgICAgICBfKCAgICAg
-      ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICBf
-      KQogICAgICAgICAgIE9PKCAgLSBUcmFpbiBNYW5hZ2VtZW50IFNpbXVsYXRv
-      ciAtICAgICAgICAgICAgICAgICAgIClfCiAgICAgICAgICAwICAoXyAgIE1h
-      ZGUgYnkgU2VyZ2V5IERlbXlhbmVua28gZm9yIHRoZSBUaGlua25ldGljYSAg
-      XykKICAgICAgICBvMCAgICAgKF8gICAgICAgICAgICAgICAgIGNvdXJzZS4g
-      ICAgICAgICAgICAgICAgICAgICAgIF8pCiAgICAgICBvICAgICAgICAgJz0t
-      X19fLT09PS1fX19fXy09PT09PT09PS1fX19fX19fX19fXy09PT0tZHdiLT0n
-      CiAgICAgLm8gICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIF9fX19f
-      X19fXwogICAgLiBfX19fX18gICAgICAgICAgX19fX19fX19fX19fX18gIHwg
-      ICAgICAgICB8ICAgICAgX19fX18KICBfKClffHxfX3x8IF9fX19fX19fIHwg
-      ICAgICAgICAgICB8ICB8X19fX19fX19ffCAgIF9ffHxfX198fF9fCiAoICAg
-      ICAyMDI0fCB8ICAgICAgfCB8ICAgICAgICAgICAgfCBfX1lfX19fX18wMF98
-      IHxfICAgICAgICAgX3wKLy1PTy0tLS1PTyIiPSJPTy0tT08iPSJPTy0tLS0t
-      LS0tT08iPSJPTy0tLS0tLS1PTyI9Ik9PLS0tLS0tLU9PIj1QCiAjIyMjIyMj
-      IyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMj
-      IyMjIyMjIyMjIyMjIyMjIyMK
+      eJytUT1vwjAQ3f0rTmGADE4t1DVbV+RKsHQ6GXAhgthSGoQi3Y+vYwfXSZBY
+      +qTk3n28l4sNMAXy0oFjiCnzHJN0KLGJwWrm+QqYpx5SOgcOu0ZVBjbKqJOu
+      tWlhW9W3q2pt45pz5OkeAmCFLm7UUcO+g61uTrqDD113ymhzsfDtbNqzht25
+      Mhej2+qgRntY4YN3GeNgb82PLl7+io21pT84Pj/AkGIs8uN9z8tlcCjsE/vx
+      tx7wgmLIn/SHOsUOJQOsv7IcidA9UZMMh3mKRuSV/bx/MQgXvhbrd3rIaKpH
+      /ApqIZwB/W2JxN64lNxByiwrs547FkjAOJ1nnwwW/wH2Cxl/p/o=
     ABOUT
-    Base64.decode64(content)
+    Zlib::Inflate.inflate(Base64.decode64(content))
   end
 end
 # rubocop:enable all
